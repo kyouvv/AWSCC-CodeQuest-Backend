694cb888441b516a69d13fa0b3be2de77289d425
kyouvv <allenalcorano@gmail.com>
2023-11-22 10:50:22 +0800

    Update day-2.py

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,5 @@
+firstNumber = int(input("Enter the First Number: "))
+secondNumber = int(input("Enter the Second Number: "))
+thirdNumber = int(input("Enter the Third Number: "))
+
+print(f"Sum: {firstNumber+secondNumber+thirdNumber}")
