0f4fd2ce9bfb2bd469bf03994acbd114a2f4b36f
kyouvv <allenalcorano@gmail.com>
2023-11-25 03:30:18 +0800

    Day 11

---
+++ backend/day-11/day-11-challenge.py
@@ -0,0 +1,24 @@
+def sorter(name_list):
+    name_list.sort()
+    for i in range(len(name_list)):
+        print(f"{i}. {name_list[i].strip()}")
+
+def fileRead():
+    global students
+    with open("students.txt", "r") as file:
+        students = [line.strip() for line in file.readlines()]
+    return students
+
+def fileWrite():
+    global students
+    with open("students.txt", "w") as file:
+        for student in students:
+            file.write(student + '\n')
+
+def main():
+    global students
+    students = fileRead()
+    sorter(students)
+    fileWrite()
+
+main()
