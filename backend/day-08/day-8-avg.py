2700d59ccb8fd8b15998b9ef9f9bac6c8c5a79c1
kyouvv <allenalcorano@gmail.com>
2023-11-24 12:37:51 +0800

    Day 8
    Learned alot about debugging and how functions work and how they are used in real programs.

--- backend/day-08/day-8-avg.py
+++ backend/day-08/day-8-avg.py
@@ -1,10 +1,13 @@
+def avg(numlist):
+    sum = 0
+    vg = 0
+
+    for num in my_list:
+        sum += num
+
+    avg = sum / len(my_list)
+    return avg
+
 my_list = [5, 9, 2, 3, 1]
-
-sum = 0
-avg = 0
-
-for num in my_list:
-    sum += num
-
-avg = sum / len(my_list)
-print(avg)
+my_avg = avg(my_list)
+print(my_avg)
