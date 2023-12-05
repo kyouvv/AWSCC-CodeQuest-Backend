3bf5b2ca560cc8a3e9e62b0de1ba5b56e03a5bda
kyouvv <allenalcorano@gmail.com>
2023-12-05 21:10:19 +0800

    a

--- backend/day-17/day-17-challenge.py
+++ backend/day-17/day-17-challenge.py
@@ -2,4 +2,7 @@ import requests
 
 data = requests.get('https://jsonplaceholder.typicode.com/')
 
-print(data)
+if data.status_code == 200:
+    print('Request Succesful.')
+else:
+    print('Request Failed.')
